5de71fc46e61e140e2af21aa0d9f33065d63a40e
oneder2 <gellar@tutanota.com>
2026-01-06 11:02:41 -0800

    fix: 修复LinkIcon.vue文件为空的问题

---
+++ src/components/icons/LinkIcon.vue
@@ -0,0 +1,11 @@
+<!--
+  友站链接图标
+  用于表示友情链接或外部链接
+-->
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/>
+    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/>
+  </svg>
+</template>
+
