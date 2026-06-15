b5019b1facb3ee874d064797b07a0b263bdb60cf
oneder2 <gellar@tutanota.com>
2026-06-16 01:11:05 +0800

    Allow PATCH in backend CORS preflight

--- backend/src/server.js
+++ backend/src/server.js
@@ -57,7 +57,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }))
 
